c4ef7dc28bc35e2789048bcd8eded861916d057c
Joao Pedro Cordeiro <jpcordeiro@Joao-Pedros-MacBook-Pro.local>
2022-01-17 00:44:46 -0300

    Refactoring

---
+++ src/components/Food/index.tsx
@@ -0,0 +1,114 @@
+import { useState } from "react";
+import { FiEdit3, FiTrash } from "react-icons/fi";
+
+import { Container } from "./styles";
+import api from "../../services/api";
+
+interface FoodArrayProps {
+  available: boolean;
+  description: string;
+  id: number;
+  image: string;
+  name: string;
+  price: string;
+}
+
+interface FoodProps {
+  food: FoodArrayProps;
+  handleDelete: (food: FoodArrayProps["id"]) => void;
+  handleEditFood: (food: FoodArrayProps) => void;
+}
+
+const Food = ({ food, handleDelete, handleEditFood }: FoodProps) => {
+  const [isAvailable, setIsAvailable] = useState(food.available);
+
+  const toggleAvailable = async () => {
+    await api.put(`/foods/${food.id}`, {
+      ...food,
+      available: !isAvailable,
+    });
+
+    setIsAvailable(!isAvailable);
+  };
+
+  return (
+    <Container available={isAvailable}>
+      <header>
+        <img src={food.image} alt={food.name} />
+      </header>
+      <section className="body">
+        <h2>{food.name}</h2>
+        <p>{food.description}</p>
+        <p className="price">
+          R$ <b>{food.price}</b>
+        </p>
+      </section>
+      <section className="footer">
+        <div className="icon-container">
+          <button
+            type="button"
+            className="icon"
+            onClick={() => handleEditFood(food)}
+            data-testid={`edit-food-${food.id}`}
+          >
+            <FiEdit3 size={20} />
+          </button>
+
+          <button
+            type="button"
+            className="icon"
+            onClick={() => handleDelete(food.id)}
+            data-testid={`remove-food-${food.id}`}
+          >
+            <FiTrash size={20} />
+          </button>
+        </div>
+
+        <div className="availability-container">
+          <p>{isAvailable ? "Disponível" : "Indisponível"}</p>
+
+          <label htmlFor={`available-switch-${food.id}`} className="switch">
+            <input
+              id={`available-switch-${food.id}`}
+              type="checkbox"
+              checked={isAvailable}
+              onChange={toggleAvailable}
+              data-testid={`change-status-food-${food.id}`}
+            />
+            <span className="slider" />
+          </label>
+        </div>
+      </section>
+    </Container>
+  );
+};
+
+export default Food;
+
+// class Food extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     const { available } = this.props.food;
+//     this.state = {
+//       isAvailable: available,
+//     };
+//   }
+
+//   toggleAvailable = async () => {
+//     const { food } = this.props;
+//     const { isAvailable } = this.state;
+
+//     await api.put(`/foods/${food.id}`, {
+//       ...food,
+//       available: !isAvailable,
+//     });
+
+//     this.setState({ isAvailable: !isAvailable });
+//   };
+
+//   setEditingFood = () => {
+//     const { food, handleEditFood } = this.props;
+
+//     handleEditFood(food);
+//   };
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,19 +1,19 @@
-import {
-  useEffect,
-  useRef,
-  useState,
-  useCallback,
-} from 'react';
+import { useEffect, useRef, useState, useCallback } from "react";
 
-import { useField } from '@unform/core';
+import { useField } from "@unform/core";
 
-import { Container } from './styles';
+import { Container } from "./styles";
 
-const Input = ({ name, icon: Icon, ...rest }) => {
+interface InputProps {
+  name: string;
+  icon: any;
+}
+
+const Input = ({ name, icon: Icon, ...rest }: InputProps) => {
   const inputRef = useRef(null);
 
   const [isFocused, setIsFocused] = useState(false);
-  const [isFilled, setIsFilled] = useState(false);
+  const [isFilled, setIsFilled] = useState<any>(false);
 
   const { fieldName, defaultValue, registerField } = useField(name);
 
@@ -24,14 +24,14 @@ const Input = ({ name, icon: Icon, ...rest }) => {
   const handleInputBlur = useCallback(() => {
     setIsFocused(false);
 
-    setIsFilled(!!inputRef.current?.value);
+    setIsFilled(!!inputRef.current);
   }, []);
 
   useEffect(() => {
     registerField({
       name: fieldName,
       ref: inputRef.current,
-      path: 'value',
+      path: "value",
     });
   }, [fieldName, registerField]);
 
