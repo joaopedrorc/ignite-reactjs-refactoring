823ffb0ad7c0d9d29a416e545395b456d57c7293
jpc5626@gmail.com <jpc5626@gmail.com>
2022-03-06 01:00:30 -0300

    fix: header component refactory

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { FiPlusSquare } from "react-icons/fi";
+
+import { Container, HeaderDiv, Button, ButtonText, ButtonIcon } from "./styles";
+import Logo from "../../assets/logo.svg";
+
+interface HeaderProps {
+  openModal: () => {};
+}
+
+const Header = ({ openModal }: HeaderProps) => {
+  return (
+    <Container>
+      <HeaderDiv>
+        <img src={Logo} alt="GoRestaurant" />
+        <nav>
+          <Button type="button" onClick={openModal}>
+            <ButtonText className="text">Novo Prato</ButtonText>
+            <ButtonIcon className="icon">
+              <FiPlusSquare size={24} />
+            </ButtonIcon>
+          </Button>
+        </nav>
+      </HeaderDiv>
+    </Container>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background: #c72828;
+  padding: 30px 0;
+`;
+
+export const HeaderDiv = styled.header`
+  width: 1280px;
+  margin: 0 auto;
+  padding: 0 0 160px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Button = styled.button`
+  font-weight: 600;
+  border-radius: 8px;
+  border: 0;
+  background: #39b100;
+  color: #fff;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const ButtonText = styled.div`
+  padding: 16px 24px;
+`;
+
+export const ButtonIcon = styled.div`
+  display: flex;
+  padding: 16px 16px;
+  background: #41c900;
+  border-radius: 0 8px 8px 0;
+  margin: 0 auto;
+`;
